e7bffb2c9c8e95ec608f358e7c3a823c488b3841
山本　泰弘 <y-yamamoto@yworks.net>
2020-09-23 22:37:32 +0900

    設定ファイルにライブラリの記述を追加

--- src/vacancy/vacancy/settings.py
+++ src/vacancy/vacancy/settings.py
@@ -33,6 +33,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'rest_framework',
+    'django_filters',
     'api',
     'building',
     'documents',
