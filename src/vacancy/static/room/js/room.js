02ea07d2c644ab3dcdeef69454f6a5dc6319ff1a
山本　泰弘 <y-yamamoto@yworks.net>
2020-11-11 19:40:01 +0900

    建物詳細と部屋詳細の地図のアイコンの変更

--- src/vacancy/static/room/js/room.js
+++ src/vacancy/static/room/js/room.js
@@ -20,6 +20,11 @@ function createRoomVue(
             url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
             attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
             marker: L.latLng(lat, lng),
+            icon: L.icon({
+                    iconUrl: "/static/vacancy/images/building_icon.png",
+                    iconSize: [48, 48],
+                    iconAnchor: [24, 24],
+            }),
         },
         mounted: function(event) {
         },
